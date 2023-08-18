589870ef692f6d9b2bb5f0e33fea106f1cc7a5b9
Mikael Elias <129218135+mikaelelias@users.noreply.github.com>
2023-08-17 22:22:39 -0300

    teste

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -4,12 +4,19 @@ const CampoTexto = (props) => {
 
     const placeholderModificada = `${props.placeholder}...` 
 
+    let valor = 'Mikael Elias'
+
+    const aoDigitado = (evento) => {
+        valor = evento.target.valor
+        console.log(value)
+    }
+
     return (
         <div className="campo-texto">
             <label>
                 {props.label}
             </label>
-            <input placeholder={placeholderModificada}/>
+            <input value={valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={placeholderModificada}/>
         </div>
     )
 }
